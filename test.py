33b51bb785570b0775bd67e01ce983d740c6548c
jstne-r <dtxn.132@gmail.com>
2023-11-20 21:40:54 +0800

    calculator python test

---
+++ test.py
@@ -0,0 +1,34 @@
+
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+def main():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Enter the operation (+, -, *, /): ")
+
+    if operation == "+":
+        result = add(num1, num2)
+    elif operation == "-":
+        result = subtract(num1, num2)
+    elif operation == "*":
+        result = multiply(num1, num2)
+    elif operation == "/":
+        result = divide(num1, num2)
+    else:
+        print("Invalid operation")
+        return
+
+    print("Result:", result)
+
+if __name__ == "__main__":
+    main()
